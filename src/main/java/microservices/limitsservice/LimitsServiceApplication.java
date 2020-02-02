417cbf76794936aa27ec613e4aa7b53def00aa9a
Alvaro Rodrigues <alvarorodrigues1710@gmail.com>
2020-02-02 21:02:16 +0000

    enabled eureka config

--- src/main/java/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/microservices/limitsservice/LimitsServiceApplication.java
@@ -3,9 +3,11 @@ package microservices.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class LimitsServiceApplication {
 
     public static void main(String[] args) {
